c576d87346c5f2d08de26f22aab6dff6d055d520
test <test@test.com>
2024-07-03 00:11:05 +1000

    update css & js

--- cikm24/js/script.js
+++ cikm24/js/script.js
@@ -1,6 +1,6 @@
 function toggleSubMenu() {
     var subMenu = document.getElementById('infoSubMenu');
-    if (subMenu.style.display === 'block' || subMenu.style.display === '') {
+    if (subMenu.style.display === 'block') {
         subMenu.style.display = 'none';
     } else {
         subMenu.style.display = 'block';
