f1c1028c932296253ffbc280b5f8422ccc2edcdc
Huaming-Chen <favorming@gmail.com>
2024-07-31 15:44:55 +1000

    update acm-bcb24

---
+++ acm-bcb24/js/script.js
@@ -0,0 +1,8 @@
+function toggleSubMenu() {
+    var subMenu = document.getElementById('infoSubMenu');
+    if (subMenu.style.display === 'block') {
+        subMenu.style.display = 'none';
+    } else {
+        subMenu.style.display = 'block';
+    }
+}
